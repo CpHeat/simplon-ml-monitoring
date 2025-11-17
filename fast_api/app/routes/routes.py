4ea25d9e4632bfbf4ed46a5ba8e82c3e3f3f8080
sayana_project <sayana.ear@gmail.com>
2025-11-17 16:10:42 +0100

    test model ml

--- fast_api/app/routes/routes.py
+++ fast_api/app/routes/routes.py
@@ -1,62 +1,19 @@
 from fastapi import APIRouter,HTTPException,Query
 from app.schemas import InputMachine, InputDeep
-# from app.predict import Predict
+from app.predict import Predict
 
 router = APIRouter(prefix="/predict")
-# predict = Predict()
+predictor = Predict()
 
-@router.post("/ml",summary="Prediction si en fonction du genre taux de chance de survie machine learning",
-           description="connaitre le taux de chance de survie en fonction de différent parametre")
-async def predict_survibality_machine(data: InputMachine,genre: str = Query(..., description="homme ou femme")):
-    """
-    Renvoie la prédiction de taux de survie en fonction de différents parametre.
-    
-    Returns:
-        float: pourcentage de survie
-        
-    Raises:
-        HTTPException: Si des données manquantes
-    """
-    genre=genre.lower()
-    rate_survive=0
+@router.post("/ml")
+async def predict_ml(data: InputMachine):
     try:
-        if(genre=="homme"):
-            rate_survive=25
-        elif(genre=="femme"):
-            rate_survive=75
-        else:
-           raise HTTPException(status_code=400, detail=str(e)) 
+        result = await predictor.predict_survive_ml(
+            data=data.dict(),
+            genre=data.genre
+        )
+        return result
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
-    
-    return {
-        "rate":rate_survive
-    }
-
-@router.post("/dl",summary="Prediction si en fonction du genre taux de chance de survie deep learning",
-           description="connaitre le taux de chance de survie en fonction de différent parametre")
-async def predict_survibality_deep(data: InputDeep, genre: str = Query(..., description="homme ou femme")):
-    """
-    Renvoie la prédiction de taux de survie en fonction de différents parametre.
-    
-    Returns:
-        float: pourcentage de survie
-        
-    Raises:
-        HTTPException: Si des données manquantes
-    """
-    genre=genre.lower()
-    rate_survive=0
-    try:
-        if(genre=="homme"):
-            rate_survive=20
-        elif(genre=="femme"):
-            rate_survive=80
-        else:
-           raise HTTPException(status_code=400, detail=str(e)) 
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    
-    return {
-        "rate":rate_survive
-    }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
